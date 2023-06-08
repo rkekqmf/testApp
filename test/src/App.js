b3a4357181d9d0f2a78c5759bf642b36743e8ae5
rkekqmf <rkekqmf@gmail.com>
2023-06-08 15:03:49 +0900

    clipboard.writeText()

--- test/src/App.js
+++ test/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+  const [copyData, setCopyData] = useState("Webview용 clipboard test")
+
+  const changeText = (text) => {
+    setCopyData(text);
+  }
+  const onCopy = () => {
+    navigator.clipboard.writeText(copyData);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          앙기모띠
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <button onClick={onCopy}>버튼이요</button>
+        <input type="textfield" placehoder="clipboard check" onChange={(e)=>{changeText(e.target.value)}}></input>
+        <div>{copyData}</div>
       </header>
+
     </div>
   );
 }
