c356fae0cc15af3bbab7a84e025b116be7f43568
rkekqmf <rkekqmf@gmail.com>
2023-06-08 17:11:14 +0900

    permission wr

--- test/src/App.js
+++ test/src/App.js
@@ -2,20 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react'
 function App() {
-
   const [copyData, setCopyData] = useState("Webview용 clipboard test")
+  
 
   const doCopy=() => {
-    navigator.permissions.query({name:"clipboard-write"}).then((permission)=> {
-      console.log(permission.state);
-    });
-
-      navigator.clipboard.writeText(copyData)
+    navigator.permissions.query({ name: "clipboard-write" }).then(p => console.log(p.state))
+    
+     navigator.clipboard.writeText(copyData)
       .then(()=> {
         alert("클립보드에 복사되었습니다.");
       })
       .catch(()=>alert("하"))
-      
   };
 
   
@@ -29,7 +26,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={doCopy()}>버튼이요</button>
+        <button onClick={()=>{doCopy()}}>버튼이요</button>
         <input type="textfield" placehoder="clipboard check" onChange={(e)=>{changeText(e.target.value)}}></input>
         <div id={'cliptext'}>copyData : {copyData}</div>
       </header>
