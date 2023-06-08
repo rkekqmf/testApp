082c66bf6e8b934cf9b694a77a1bd41c691876ab
rkekqmf <rkekqmf@gmail.com>
2023-06-08 16:27:46 +0900

    Update App.js

--- test/src/App.js
+++ test/src/App.js
@@ -5,26 +5,11 @@ function App() {
 
   const [copyData, setCopyData] = useState("Webview용 clipboard test")
 
-  const doCopy = text => {
-
-      // 흐름 3.
-      const textarea = document.createElement("textarea");
-      textarea.value = copyData;
-      textarea.style.top = 0;
-      textarea.style.left = 0;
-      textarea.style.position = "fixed";
-
-      // 흐름 4.
-      document.body.appendChild(textarea);
-      // focus() -> 사파리 브라우저 서포팅
-      textarea.focus();
-      // select() -> 사용자가 입력한 내용을 영역을 설정할 때 필요
-      textarea.select();
-      // 흐름 5.
-      document.execCommand("copy");
-      // 흐름 6.
-      document.body.removeChild(textarea);
-      alert("클립보드에 복사되었습니다.");
+  const doCopy=() => {
+      navigator.clipboard.writeText(copyData).then(()=> {
+        alert("클립보드에 복사되었습니다.");
+      }).catch(()=>alert("하"))
+      
   };
 
   
