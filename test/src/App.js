a02c78348fa87b4210e1442bb9c2d6d8ac355760
rkekqmf <rkekqmf@gmail.com>
2023-06-08 15:34:03 +0900

    DDD

--- test/src/App.js
+++ test/src/App.js
@@ -4,22 +4,58 @@ import { useState } from 'react'
 
 function App() {
 
+  const doCopy = text => {
+    // 흐음 1.
+    if (navigator.clipboard) {
+      // (IE는 사용 못하고, 크롬은 66버전 이상일때 사용 가능합니다.)
+      navigator.clipboard
+        .writeText(text)
+        .then(() => {
+          alert("클립보드에 복사되었습니다.");
+        })
+        .catch(() => {
+          alert("복사를 다시 시도해주세요.");
+        });
+    } else {
+      // 흐름 2.
+      if (!document.queryCommandSupported("copy")) {
+        return alert("복사하기가 지원되지 않는 브라우저입니다.");
+      }
+
+      // 흐름 3.
+      const textarea = document.createElement("textarea");
+      textarea.value = text;
+      textarea.style.top = 0;
+      textarea.style.left = 0;
+      textarea.style.position = "fixed";
+
+      // 흐름 4.
+      document.body.appendChild(textarea);
+      // focus() -> 사파리 브라우저 서포팅
+      textarea.focus();
+      // select() -> 사용자가 입력한 내용을 영역을 설정할 때 필요
+      textarea.select();
+      // 흐름 5.
+      document.execCommand("copy");
+      // 흐름 6.
+      document.body.removeChild(textarea);
+      alert("클립보드에 복사되었습니다.");
+    }
+  };
+
   const [copyData, setCopyData] = useState("Webview용 clipboard test")
 
   const changeText = (text) => {
     setCopyData(text);
   }
-  const onCopy = () => {
-    navigator.clipboard.write(copyData).then(
-    ()=>{alert("성공")}
-  );
-  }
+ 
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       
-        <button onClick={onCopy}>버튼이요</button>
+        <button onClick={doCopy}>버튼이요</button>
         <input type="textfield" placehoder="clipboard check" onChange={(e)=>{changeText(e.target.value)}}></input>
         <div id={'cliptext'}>copyData : {copyData}</div>
       </header>
