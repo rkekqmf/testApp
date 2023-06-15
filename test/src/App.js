6827b3c2e764af9976de1ac47be7d1a4d43b1ff8
rkekqmf <rkekqmf@gmail.com>
2023-06-15 17:35:24 +0900

    Update App.js

--- test/src/App.js
+++ test/src/App.js
@@ -6,18 +6,19 @@ function App() {
   
 
   const doCopy=() => {
-    var copyText = document.getElementById("myInput");
-    copyText.focus();
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    navigator.clipboard
-      .writeText(copyText.value)
-      .then(() => {
-        alert("successfully copied");
-      })
-      .catch(() => {
-        alert("something went wrong");
-      });
+    // (window as any).Android.copyToClipboard(text);
+    window.TestInterface = {
+      testWorld: () => {
+        navigator.clipboard
+        .writeText(copyData)
+        .then(() => {
+          alert("successfully copied");
+        })
+        .catch(() => {
+          alert("something went wrong");
+        })
+      }
+    }
   };
 
   
@@ -41,3 +42,101 @@ function App() {
 }
 
 export default App;
+
+
+
+// function copy(text) {
+//   return new Promise((resolve, reject) => {
+//       if (typeof navigator !== "undefined" && typeof navigator.clipboard !== "undefined" && navigator.permissions !== "undefined") {
+//           const type = "text/plain";
+//           const blob = new Blob([text], { type });
+//           const data = [new ClipboardItem({ [type]: blob })];
+//           navigator.permissions.query({name: "clipboard-write"}).then((permission) => {
+//               if (permission.state === "granted" || permission.state === "prompt") {
+//                   navigator.clipboard.write(data).then(resolve, reject).catch(reject);
+//               }
+//               else {
+//                   reject(new Error("Permission not granted!"));
+//               }
+//           });
+//       }
+//       else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+//           var textarea = document.createElement("textarea");
+//           textarea.textContent = text;
+//           textarea.style.position = "fixed";
+//           textarea.style.width = '2em';
+//           textarea.style.height = '2em';
+//           textarea.style.padding = 0;
+//           textarea.style.border = 'none';
+//           textarea.style.outline = 'none';
+//           textarea.style.boxShadow = 'none';
+//           textarea.style.background = 'transparent';
+//           document.body.appendChild(textarea);
+//           textarea.focus();
+//           textarea.select();
+//           try {
+//               document.execCommand("copy");
+//               document.body.removeChild(textarea);
+//               resolve();
+//           }
+//           catch (e) {
+//               document.body.removeChild(textarea);
+//               reject(e);
+//           }
+//       }
+//       else {
+//           reject(new Error("None of copying methods are supported by this browser!"));
+//       }
+//   });
+  
+// }
+
+
+
+
+
+
+// export default function ClipboardCopy() {
+//   const doCopy = text => {
+//     // 흐음 1.
+//     if (navigator.clipboard) {
+//       // (IE는 사용 못하고, 크롬은 66버전 이상일때 사용 가능합니다.)
+//       navigator.clipboard
+//         .writeText(text)
+//         .then(() => {
+//           alert("클립보드에 복사되었습니다.");
+//         })
+//         .catch(() => {
+//           alert("복사를 다시 시도해주세요.");
+//         });
+//     } else {
+//       // 흐름 2.
+//       if (!document.queryCommandSupported("copy")) {
+//         return alert("복사하기가 지원되지 않는 브라우저입니다.");
+//       }
+
+//       // 흐름 3.
+//       const textarea = document.createElement("textarea");
+//       textarea.value = text;
+//       textarea.style.top = 0;
+//       textarea.style.left = 0;
+//       textarea.style.position = "fixed";
+
+//       // 흐름 4.
+//       document.body.appendChild(textarea);
+//       // focus() -> 사파리 브라우저 서포팅
+//       textarea.focus();
+//       // select() -> 사용자가 입력한 내용을 영역을 설정할 때 필요
+//       textarea.select();
+//       // 흐름 5.
+//       document.execCommand("copy");
+//       // 흐름 6.
+//       document.body.removeChild(textarea);
+//       alert("클립보드에 복사되었습니다.");
+//     }
+//   };
+
+//   return (
+//     <button onClick={() => doCopy("복사할텍스트입니다!")}>복사하기</button>
+//   );
+// }
